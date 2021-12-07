e999ab064d0430d560e6a1d9142f92f90262293d
SoonKi Kwon <jhk327@gmail.com>
2021-12-08 02:02:55 +0900

    create App.js, index.js

---
+++ ssr/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import Home from './Home';
+import About from './About';
+
+export default function App({ page }) {
+  const [page, setPage] = useState(page);
+  useEffect(() => {
+    window.onpopstate = (event) => {
+      setPage(event.state);
+    };
+  }, []);
+
+  const onChangePage = (e) => {
+    const newPage = e.target.dataset.page;
+    window.history.pushState(newPage, '', `/${newPage}`);
+    setPage(newPage);
+  };
+
+  const PageComponent = page === 'home' ? Home : About;
+
+  return (
+    <div className="container">
+      <button data-page="home" onClick={onChangePage}>
+        Home
+      </button>
+      <button data-page="about" onClick={onChangePage}>
+        About
+      </button>
+      <PageComponent />
+    </div>
+  );
+}
