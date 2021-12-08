c8790d7241527e8d945db0c3d8697ebe97849d92
SoonKi Kwon <jhk327@gmail.com>
2021-12-09 02:36:32 +0900

    create server for ssr

--- ssr/src/server.js
+++ ssr/src/server.js
@@ -9,6 +9,7 @@ const app = express();
 const html = fs.readFileSync(path.resolve(__dirname, '../dist/index.html'), 'utf8');
 
 app.use('/dist', express.static('dist'));
+// 브라우저가 자동으로 요청하는 favicon.ico파일을 필터링한다.
 app.get('/favicon.ico', (req, res) => res.sendStatus(204));
 app.get('*', (req, res) => {
   const renderString = renderToString(<App initialPage="home" />);
