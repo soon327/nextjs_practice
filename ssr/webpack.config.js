3b67c77e43d988d14e64811e7533c45b8f30908b
SoonKi Kwon <jhk327@gmail.com>
2021-12-09 20:05:52 +0900

    clean-webpack-plugin

--- ssr/webpack.config.js
+++ ssr/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -23,6 +24,6 @@ module.exports = {
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({ template: './template/index.html' })],
+  plugins: [new CleanWebpackPlugin(), new HtmlWebpackPlugin({ template: './template/index.html' })],
   mode: 'production',
 };
