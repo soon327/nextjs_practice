d91b778f96c9fa4679874229d215d09d630d62ca
SoonKi Kwon <jhk327@gmail.com>
2021-12-09 19:16:15 +0900

    update package.json scripts

--- ssr/src/index.js
+++ ssr/src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App initialPage="home" />, document.getElementById('root'));
+// render 대신 hydrate함수 사용. hydrate함수는 서버사이드 렌더링의 결과로 만들어진 돔 요소에 필요한 이벤트처리함수를 붙여준다.
+ReactDOM.hydrate(<App initialPage="home" />, document.getElementById('root'));
