69389748c470e46edfc4dd791ebc8635d4ef39d8
SoonKi Kwon <jhk327@gmail.com>
2021-12-09 19:54:55 +0900

    style_styled components for ssr

--- ssr/src/App.js
+++ ssr/src/App.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import Home from './Home';
 import About from './About';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  background-color: #aaaaaa;
+  border: 1px solid blue;
+`;
 
 export default function App({ initialPage }) {
   const [page, setPage] = useState(initialPage);
@@ -19,7 +25,7 @@ export default function App({ initialPage }) {
   const PageComponent = page === 'home' ? Home : About;
 
   return (
-    <div className="container">
+    <Container>
       <button data-page="home" onClick={onChangePage}>
         Home
       </button>
@@ -27,6 +33,6 @@ export default function App({ initialPage }) {
         About
       </button>
       <PageComponent />
-    </div>
+    </Container>
   );
 }
--- ssr/src/server.js
+++ ssr/src/server.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import * as url from 'url';
 import { renderToString } from 'react-dom/server';
+import { ServerStyleSheet } from 'styled-components';
 import React from 'react';
 import App from './App';
 
@@ -16,13 +17,18 @@ app.get('*', (req, res) => {
   const parsedUrl = url.parse(req.url, true);
   // substr 메서드로 슬래쉬(/) 제거
   const page = parsedUrl.pathname ? parsedUrl.pathname.substr(1) : 'home';
-
-  const renderString = renderToString(<App initialPage={page} />);
+  // 스타일을 추출하는 데 사용될 객체
+  const sheet = new ServerStyleSheet();
+  // collectStyles 메서드로 스타일정보를 수집하기위한 코드를 리액트요소에 삽입한다.
+  const renderString = renderToString(sheet.collectStyles(<App initialPage={page} />));
+  // hetStyleTags 메서드로 스타일정보를 추출한다.
+  const styles = sheet.getStyleTags();
   const initialData = { page };
   const result = html
     .replace('<div id="root"></div>', `<div id="root">${renderString}</div>`)
     // __DATA_FROM_SERVER__ 문자열을 초기데이터로 대체한다.
-    .replace('__DATA_FROM_SERVER__', JSON.stringify(initialData));
+    .replace('__DATA_FROM_SERVER__', JSON.stringify(initialData))
+    .replace('__STYLE_FROM_SERVER__', styles);
   res.send(result);
 });
 app.listen(3000);
