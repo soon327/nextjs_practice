da6f4be801d61a89c21348b34161f353f31643a5
SoonKi Kwon <jhk327@gmail.com>
2021-12-19 23:37:59 +0900

    update page2

--- nextjs/pages/page2.js
+++ nextjs/pages/page2.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { callApi } from '../src/api';
+import Router from 'next/router';
 
 page2.getInitialProps = async ({ query }) => {
   const text = query.text || 'none';
@@ -9,10 +10,15 @@ page2.getInitialProps = async ({ query }) => {
 
 export default function page2({ text, data }) {
   return (
-    <div>
-      <p>this is page2 for studing getInitialProps</p>
-      <p>{`text: ${text}`}</p>
-      <p>{`data: ${data}`}</p>
-    </div>
+    <>
+      <div>
+        <p>this is page2 for studing getInitialProps</p>
+        <p>{`text: ${text}`}</p>
+        <p>{`data: ${data}`}</p>
+      </div>
+      <div>
+        <button onClick={() => Router.push('/homepage')}>homepage로 이동</button>
+      </div>
+    </>
   );
 }
