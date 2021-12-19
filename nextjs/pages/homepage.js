04c5c989bf511480b3f4155421ca6e67dd54d981
SoonKi Kwon <jhk327@gmail.com>
2021-12-19 23:35:32 +0900

    update homepage

--- nextjs/pages/homepage.js
+++ nextjs/pages/homepage.js
@@ -2,11 +2,15 @@
 // import React from 'react';
 import Head from 'next/head';
 import Icon from '../static/icon.png';
+import Link from 'next/link';
 
 export default function Homepage() {
   return (
     <div>
       <p>This is home page</p>
+      <Link href="page2">
+        <a>page2로 이동</a>
+      </Link>
       <img src={Icon} width="300" height="300" />
       <Head>
         <title>homepage</title>
