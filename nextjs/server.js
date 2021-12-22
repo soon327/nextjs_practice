501d3cdbd5042bc32854e34ad63d5f5d9c0a1904
SoonKi Kwon <jhk327@gmail.com>
2021-12-22 23:51:49 +0900

    create server

---
+++ nextjs/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const next = require('next');
+
+const port = 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+//넥스트 준비과정이 끝나면 입력함수를 실행한다.
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('/page/:id', (req, res) => {
+    res.redirect(`/page${req.params.id}`);
+  });
+  server.get('*', (req, res) => {
+    return handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`);
+  });
+});
