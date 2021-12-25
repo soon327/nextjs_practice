f441c4009a265ff0e8df14939540a92e230d590c
SoonKi Kwon <jhk327@gmail.com>
2021-12-25 23:14:43 +0900

    update server for caching

--- nextjs/server.js
+++ nextjs/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const next = require('next');
+const lruCache = require('lru-cache');
 
 const port = 3000;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+const ssrCache = new lruCache({
+  max: 100,
+  maxAge: 1000 * 60,
+});
 
 //넥스트 준비과정이 끝나면 입력함수를 실행한다.
 app.prepare().then(() => {
@@ -13,9 +19,30 @@ app.prepare().then(() => {
   server.get('/page/:id', (req, res) => {
     res.redirect(`/page${req.params.id}`);
   });
+
+  server.get('/^/page[1-9]/', (req, res) => renderAndCache(req, res));
   server.get('*', (req, res) => {
     return handle(req, res);
   });
+
+  async function renderAndCache(req, res) {
+    const parsedUrl = url.parse(req.url, true);
+    const cacheKey = parsedUrl.path;
+    if (ssrCache.has(cacheKey)) {
+      console.log('캐시사용');
+      return res.send(ssrCache.get(cacheKey));
+    }
+    try {
+      const { query, pathname } = parsedUrl;
+      const html = await app.renderToHtml(req, res, pathname, query);
+      if (res.statusCode === 200) {
+        ssrCache.set(cacheKey, html);
+      }
+      res.send(html);
+    } catch (err) {
+      app.renderError(err, req, res, pathname, query);
+    }
+  }
 
   server.listen(port, (err) => {
     if (err) throw err;
