295f580db4707374a1170533842f7e843bd91f2c
SoonKi Kwon <jhk327@gmail.com>
2022-01-03 23:13:52 +0900

    fix _document

--- nextjs/pages/_document.js
+++ nextjs/pages/_document.js
@@ -7,21 +7,20 @@ export default class MyDocument extends Document {
     const originalRenderPage = ctx.renderPage;
 
     try {
-      ctx.renderPage = () => {
+      ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
         });
 
-        const initialProps = await Document.getInitialProps(ctx);
-        return {
-          ...initialProps,
-          style: (
-            <>
-              {initialProps.styles}
-              {sheet.getStyleElement()}
-            </>
-          ),
-        };
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
       };
     } finally {
       sheet.seal();
