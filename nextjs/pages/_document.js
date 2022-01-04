c1fb424d19df4e9be65bf635a424544a17c9eef9
SoonKi Kwon <jhk327@gmail.com>
2022-01-05 00:05:53 +0900

    update metadata

--- nextjs/pages/_document.js
+++ nextjs/pages/_document.js
@@ -30,7 +30,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <title>next practice</title>
+        </Head>
         <body>
           <Main />
           <NextScript />
