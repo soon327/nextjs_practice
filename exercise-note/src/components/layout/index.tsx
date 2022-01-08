a0f5f905306d45e0cf2feb249d5f0b807f9e80b5
SoonKi Kwon <jhk327@gmail.com>
2022-01-08 21:39:20 +0900

    styling nav

--- exercise-note/src/components/layout/index.tsx
+++ exercise-note/src/components/layout/index.tsx
@@ -4,12 +4,12 @@ import Sidebar from './Sidebar';
 
 export default function index({ children }: React.PropsWithChildren<{}>) {
   return (
-    <div className="flex border-2 border-sky-500 h-screen">
+    <div className="flex flex-col border-2 border-sky-500 h-screen">
       <Head>
         <title>운동일지</title>
       </Head>
       <Sidebar />
-      <main className="flex-5 border border-red-400 w-full">{children}</main>
+      <main className="flex-8  w-full h-full">{children}</main>
     </div>
   );
 }
--- exercise-note/src/components/layout/sidebar.tsx
+++ exercise-note/src/components/layout/sidebar.tsx
@@ -4,19 +4,36 @@ import styled from 'styled-components';
 
 const Container = styled.nav`
   display: flex;
-  flex-direction: column;
 `;
+
+const menu = [
+  { name: '오늘먹은 칼탄단', href: '/food' },
+  { name: '운동달력', href: '/calendar' },
+  { name: '몸무게변화', href: '/weight' },
+];
+
+const setMenu = (data: { name: string; href: string }[]) => {
+  return data.map((v) => {
+    return (
+      <Link href={v.href} key={v.href}>
+        <a className="hover:bg-purple-300">{v.name}</a>
+      </Link>
+    );
+  });
+};
 
 export default function Sidebar() {
   return (
-    <Container className="flex-1 border-2 border-green-600 w-full">
-      <input placeholder="검색해주세요" />
-      <Link href="/">
-        <a>홈페이지</a>
-      </Link>
-      <Link href="/food">
-        <a>칼로리계산</a>
-      </Link>
+    <Container className=" flex-1 border-2 bg-purple-200 w-full h-full">
+      <div className="w-2/12 ">
+        <Link href="/">
+          <a className="text-base flex items-center">
+            <img src="/soon.png" className="w-16 h-16" />
+            <p className="text-blue-700 font-bold">Soon's EN</p>
+          </a>
+        </Link>
+      </div>
+      <div className="flex w-10/12 items-center justify-around font-semibold ">{setMenu(menu)}</div>
     </Container>
   );
 }
