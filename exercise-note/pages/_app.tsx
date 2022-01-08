95af27cbf73a23a93cc2b9188776bc0686cb5584
SoonKi Kwon <jhk327@gmail.com>
2022-01-08 21:37:26 +0900

    add font

--- exercise-note/pages/_app.tsx
+++ exercise-note/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../src/components/layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poor Story" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
