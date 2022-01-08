e823d5ade8d525216ad7e6a993a7d0dbdab11505
SoonKi Kwon <jhk327@gmail.com>
2022-01-08 21:37:46 +0900

    styling index

--- exercise-note/pages/index.tsx
+++ exercise-note/pages/index.tsx
@@ -1,18 +1,22 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-
+import getFoodInfo from '../api/getFoodInfo';
 const Home: NextPage = () => {
+  getFoodInfo('설렁탕');
   return (
-    <div>
+    <div className="h-full flex flex-col items-center justify-center w-full ">
       <Head>
         <title>운동노트</title>
         <meta name="description" content="private exercise note for soon" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/soon.png" />
       </Head>
 
-      <main>
-        <h1>신년맞이 운동노트</h1>
-        <p className="text-3xl font-bold underline">for Soon</p>
+      <main className="flex flex-col justify-center items-center w-2/6 h-5/6 bg-purple-200 shadow-lg shadow-purple-200 hover:shadow-purple-300 hover:-top-1">
+        <h1 className=" font-extrabold text-center text-3xl text-gray-900">신년맞이 운동노트</h1>
+        <img src="/soon.png" className=" w-8/12" />
+        <p>1. 운동 주 3회이상 가기</p>
+        <p>2. 계산된 단백질 하루 80g이상 먹기</p>
+        <p>3. 몸무게 63kg 이상 유지하기</p>
       </main>
     </div>
   );
