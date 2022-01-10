5a9b0cd2c6ad660e2735b6bdc03a930c392b1978
SoonKi Kwon <jhk327@gmail.com>
2022-01-10 23:20:58 +0900

    create searchModal

---
+++ exercise-note/src/components/common/modal/SearchModal.tsx
@@ -0,0 +1,10 @@
+import Modal from './Modal';
+import { ModalProps } from './Modal';
+
+export default function SearchModal({ open }: ModalProps) {
+  return (
+    <Modal open={open}>
+      <p>df</p>
+    </Modal>
+  );
+}
