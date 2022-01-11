062fce2040c6371c6a93aa02db505e14f3ae62ca
SoonKi Kwon <jhk327@gmail.com>
2022-01-11 22:21:31 +0900

    configure proxy

--- exercise-note/next.config.js
+++ exercise-note/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: '/api/:name',
+        destination: `http://openapi.foodsafetykorea.go.kr/api/${process.env.NEXT_PUBLIC_FOOD_KEY}/I2790/json/1/5/DESC_KOR=:name`,
+      },
+    ];
+  },
+};
