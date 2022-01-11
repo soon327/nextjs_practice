2dc710b8d49873baec20ddd13943b1a46f74ac32
SoonKi Kwon <jhk327@gmail.com>
2022-01-11 22:22:17 +0900

    update getFoodinfo

--- exercise-note/api/getFoodInfo.ts
+++ exercise-note/api/getFoodInfo.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
 export default function getFoodInfo(name: string) {
-  const URL = encodeURI(`http://openapi.foodsafetykorea.go.kr/api/${process.env.NEXT_PUBLIC_FOOD_KEY}/I2790/json/1/5/DESC_KOR=${name}`);
-
-  const result = axios.get(URL).then((res) => {
-    return res.data.I2790;
+  const result = axios.get(`/api/${name}`).then((res) => {
+    try {
+      return res.data.I2790;
+    } catch (err) {
+      console.log('getFoodInfo error');
+    }
   });
 
   return result;
