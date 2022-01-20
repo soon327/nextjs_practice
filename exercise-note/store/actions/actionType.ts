5d272e4df4b323c990325862bb264ca855dc6d3b
SoonKi Kwon <jhk327@gmail.com>
2022-01-20 23:48:31 +0900

    update store_actionType

---
+++ exercise-note/store/actions/actionType.ts
@@ -0,0 +1,3 @@
+export const FOOD_SEARCH = 'food/SEARCH' as const;
+export const FOOD_SEARCH_SUCCESS = 'food/SEARCH_SUCCESS' as const;
+export const FOOD_SEARCH_FAIL = 'food/SEARCH+FAIL' as const;
--- exercise-note/store/actions/index.ts
+++
@@ -1,3 +0,0 @@
-export const FOOD_SEARCH = 'food/SEARCH';
-export const FOOD_SEARCH_SUCCESS = 'food/SEARCH_SUCCESS';
-export const FOOD_SEARCH_FAIL = 'food/SEARCH+FAIL';
