2e5aba413902e4d39461688d14d3fcf4b38c43f1
SoonKi Kwon <jhk327@gmail.com>
2022-01-20 23:54:24 +0900

    create rootReducer

---
+++ exercise-note/store/reducers/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import food from './food';
+
+const rootReducer = combineReducers({ food });
+
+export default rootReducer;
+
+export type RootState = ReturnType<typeof rootReducer>;
