befa2e4dd384da51a53222d34ceb4b7245a69e52
SoonKi Kwon <jhk327@gmail.com>
2022-01-21 23:56:39 +0900

    create rootReducer

--- exercise-note/store/reducers/index.ts
+++ exercise-note/store/reducers/index.ts
@@ -1,8 +1,30 @@
-import { combineReducers } from 'redux';
+import { combineReducers, AnyAction, Reducer } from 'redux';
+import { HYDRATE } from 'next-redux-wrapper';
 import food from './food';
 
-const rootReducer = combineReducers({ food });
+// const rootReducer:Reducer = (state, action) => {
+//   switch (action.type) {
+//     case HYDRATE:
+//       return action.payload;
+//       default: {
+//         const combineReducer = combineReducers({ food });
 
+//         return combineReducer(state, action);
+//       }
+//     }
+//   };
+
+const rootReducer = combineReducers({
+  index: (state = {}, action) => {
+    switch (action.type) {
+      case HYDRATE:
+        return { ...(state as {}), ...action.payload };
+      default:
+        return state;
+    }
+  },
+  food,
+});
 export default rootReducer;
 
 export type RootState = ReturnType<typeof rootReducer>;
