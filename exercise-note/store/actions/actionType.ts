352e938027c3fefcf0aecdf1f6011cdb1f1ac02c
SoonKi Kwon <jhk327@gmail.com>
2022-01-26 19:40:05 +0900

    update modal actionType

--- exercise-note/store/actions/actionType.ts
+++ exercise-note/store/actions/actionType.ts
@@ -1,3 +1,7 @@
+//food
 export const FOOD_SEARCH = 'food/SEARCH' as const;
 export const FOOD_SEARCH_SUCCESS = 'food/SEARCH_SUCCESS' as const;
-export const FOOD_SEARCH_FAIL = 'food/SEARCH+FAIL' as const;
+export const FOOD_SEARCH_FAIL = 'food/SEARCH_FAIL' as const;
+
+//modal
+export const MODAL_OPEN = 'modal/open';
